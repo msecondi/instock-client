93f98c07b52cd8e7e682e08496c2e7c02846ef16
Ali Hamood <ali.hamood@gov.bc.ca>
2025-03-29 13:16:31 -0700

    Adds support for x1 and x2 images. Bigger image used for desktop and tablet.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,17 @@
 import './header.scss';
 import { Link, NavLink } from 'react-router-dom';
-import Logo from '../../assets/images/InStock-Logo_1x.png';
+import LogoX1 from '../../assets/images/InStock-Logo_1x.png';
+import LogoX2 from '../../assets/images/InStock-Logo_2x.png';
 import Button from '../Button/Button.jsx';
 
 function Header() {
     return (
         <header className="header">
             <Link className="header__logo" to="/">
-                <img className="header__logo-img" src={Logo} alt="InStock logo" />
+                <picture>
+                        <img className="header__logo-img" src={LogoX1} alt="InStock logo" />
+                        <source srcSet={LogoX2} media="(min-width: $min-tablet-breakpoint)" />
+                </picture>
             </Link>
             <nav className="header__nav">
                 <NavLink className="header__nav-link header__nav-link--right-margin" to="/">
