0d94489b98d2ba2cb1bf64f61fc1fb3c88ae5117
Ali Hamood <ali.hamood@gov.bc.ca>
2025-03-29 11:47:58 -0700

    Adds new mixin for tablet and desktop. Adds site logo and buttons to header. Adds background color to header. Adds nav links to header for both routs.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,26 @@
 import './header.scss';
+import { Link, NavLink } from 'react-router-dom';
+import Logo from '../../assets/images/InStock-Logo_1x.png';
+import Button from '../Button/Button.jsx';
 
 function Header() {
     return (
         <header className="header">
-
+            <Link className="header__logo" to="/">
+                <img className="header__logo-img" src={Logo} alt="InStock logo" />
+            </Link>
+            <NavLink className="header__nav" to="/">
+                { ({isActive}) => (
+                    <Button buttonText="Warehouse" 
+                        buttonType={isActive ? 'nav--active' : 'nav'} />
+                )}
+            </NavLink>
+            <NavLink className="header__nav" to="/inventory">
+                { ({isActive}) => (
+                    <Button buttonText="Inventory" 
+                        buttonType={isActive ? 'nav--active' : 'nav'} />
+                )}
+            </NavLink>
         </header>
     );
 }
