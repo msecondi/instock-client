47714e967293a3cdced8aeb8dd777faf6d3ef756
Ali Hamood <ali.hamood@gov.bc.ca>
2025-03-30 11:51:07 -0700

    Minor syntax clean up.

--- src/components/TableAction/TableAction.jsx
+++ src/components/TableAction/TableAction.jsx
@@ -1,16 +1,16 @@
 import './tableAction.scss';
 import Button from '../Button/Button';
-import editIcon from '../../assets/icons/edit-24px.svg';
-import deleteIcon from '../../assets/icons/delete_outline-24px.svg';
+import EditIcon from '../../assets/icons/edit-24px.svg';
+import DeleteIcon from '../../assets/icons/delete_outline-24px.svg';
 
 function TableAction({deleteAction, editAction}) {
     return (
         <div className="table-action">
             <div className="table-action__left-button">
-                <Button buttonType={'icon-only'} imgSrc={deleteIcon} onClick={deleteAction}/>
+                <Button buttonType={'icon-only'} imgSrc={DeleteIcon} onClick={deleteAction}/>
             </div>
             <div className="table-action__right-button">
-                <Button buttonType={'icon-only'} imgSrc={editIcon} onClick={editAction}/>
+                <Button buttonType={'icon-only'} imgSrc={EditIcon} onClick={editAction}/>
             </div>
         </div>
     );
