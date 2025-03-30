b365b3847c3ca6691f8224a47bbdda1dd068e7ec
Ali Hamood <ali.hamood@gov.bc.ca>
2025-03-30 13:13:39 -0700

    Adds table row component.

---
+++ src/components/TableRow/TableRow.jsx
@@ -0,0 +1,45 @@
+import './tableRow.scss';
+import TableLink from '../TableLink/TableLink';
+import TableAction from '../TableAction/TableAction';
+
+function TableRow({warehouse}) {
+    const link = {
+        url: warehouse.link,
+        text: warehouse.name
+    }
+
+    return (
+        <div className="table-row">
+            <div className="table-row__info-groups">
+                <div className="table-row__info-group-1">
+                    <div className="table-row__info">
+                        <label className="table-row__label">WAREHOUSE</label>
+                        <TableLink link={link} />
+                    </div>
+                    <div className="table-row__info">
+                        <label className="table-row__label">ADDRESS</label>
+                        <span className="table-row__address">{warehouse.address}</span>
+                    </div>
+                </div>
+                <div className="table-row__info-group-2">
+                    <div className="table-row__info">
+                        <label className="table-row__label">CONTACT NAME</label>
+                        <span className="table-row__contact-name">{warehouse.contactName}</span>
+                    </div>
+                    <div className="table-row__info">
+                        <label className="table-row__label">CONTACT INFORMATION</label>
+                        <div className="table-row__contact-info">
+                            <span className="table-row__contact-info-text">{warehouse.phoneNumber}</span>
+                            <span className="table-row__contact-info-text">{warehouse.email}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="table-row__action-group">
+                <TableAction />
+            </div>
+        </div>
+    );
+}
+
+export default TableRow;
