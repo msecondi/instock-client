bcfe8e2d14ee3cb6f433cd382e06e05851cafa69
Ali Hamood <ali.hamood@gov.bc.ca>
2025-03-29 19:03:11 -0700

    Adds formating and spacing for hero for mobile.

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,12 +1,18 @@
 import './hero.scss';
 import Button from '../Button/Button';
+import SearchIcon from '../../assets/icons/search-24px.svg';
 
 function Hero() {
     return (
         <section className="hero">
             <h1 className="hero__title">Warehouses</h1>
             <div className="hero__controls">
-                <input className="hero__search" type="text" placeholder="Search" /> 
+                <div className="hero__search-box hero__search-box--right-margin">
+                    <input className="hero__search-input" type="text" placeholder="Search..." /> 
+                    <button className="hero__search-button">
+                        <img className="hero__search-icon" src={SearchIcon} alt="Search Icon" />
+                    </button>
+                </div>
                 <Button buttonText="+ Add New Warehouse" />
             </div>
         </section>
