ac07bdde87b1fdf9a6b46d3e6789dce4fce884bb
Ali Hamood <ali.hamood@gov.bc.ca>
2025-03-30 20:11:14 -0700

    Adds navIndexes to appData, and minor changes to app.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './styles/app.scss';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 import Header from './components/Header/Header';
 import Warehouses from './pages/Warehouses/Warehouses';
 import AddWarehouse from './pages/AddWarehouse/AddWarehouse';
@@ -11,17 +12,19 @@ import Footer from './components/Footer/Footer';
 
 function App() {
 
+  const [navIndex, setNavIndex] = useState(0);
+
   return (
     <>
       <BrowserRouter>
-        <Header/>
+        <Header navIndex={navIndex}/>
         <Routes>
-            <Route path="/" element={ <Warehouses /> }/>
+            <Route path="/" element={ <Warehouses setNaveIndex={setNavIndex}/> }/>
             <Route path="/warehouses/add-warehouse" element={ <AddWarehouse /> }/>
             <Route path="/warehouses/:id" element={ <Warehouse /> }/>
             <Route path="/warehouses/:id/edit-warehouse" element={ <EditWarehouse /> }/>
             <Route path="/warehouses/:id/delete-warehouse" element={ <DeleteWarehouse /> }/>
-            <Route path="/inventory" element={ <Inventory /> }/>
+            <Route path="/inventory" element={ <Inventory setNaveIndex={setNavIndex}/> }/>
         </Routes>
         <Footer />
       </BrowserRouter>
