9027ddabfb7b6ad239c0f75cfbbb7ea64807feb6
Ali Hamood <ali.hamood@gov.bc.ca>
2025-03-31 10:06:22 -0700

    Copied Warehouses to warehouse and started removing code to match mockup. Adds two new componentes, TableHeaderWhDetails and HeroWhDetails with small modification to meet the needs for the warehouse details page.

---
+++ src/components/HeroWhDetails/HeroWhDetails.jsx
@@ -0,0 +1,25 @@
+import './heroWhDetails.scss';
+import { Link } from 'react-router-dom';
+import Button from '../Button/Button';
+import EditIcon from '../../assets/icons/edit-white-24px.svg';
+import BackIcon from '../../assets/icons/arrow_back-24px.svg';
+
+function HeroWhDetails({heroTitle, id}) {
+    return (
+        <section className="hero">
+            <div className="hero__heading">
+                <Link className="hero__link hero__link--heading" to="/">
+                    <img className="hero__back-icon" src={BackIcon} alt="Back Icon" />
+                    <h1 className="hero__title">{heroTitle ?? 'Warehouse Name not Found'}</h1>
+                </Link>
+            </div>
+            <div className="hero__controls">
+                <Link className="hero__link" to={`/warehouses/${id}/edit-warehouse`}>
+                    <Button buttonText="Edit" imgSrc={EditIcon}/>
+                </Link>
+            </div>
+        </section>
+    );
+}
+
+export default HeroWhDetails;
--- src/pages/Warehouse/Warehouse.jsx
+++ src/pages/Warehouse/Warehouse.jsx
@@ -1,17 +1,46 @@
-import './Warehouse.scss';
-import { useEffect } from 'react';
-import { warehousesPageIndex } from '../../data/appData.json';
+import './warehouse.scss';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { warehousesEndpoint, warehousesPageIndex } from '../../data/appData.json';
+import { v4 as uuidv4 } from 'uuid';
+import HeroWhDetails from '../../components/HeroWhDetails/HeroWhDetails';
+import TableHeaderWhDetails from '../../components/TableHeaderWhDetails/TableHeaderWhDetails';
+import TableRow from '../../components/TableRow/TableRow';
 
-function Warehouse({setNavIndex}) {
-  useEffect(() => {
-      setNavIndex(warehousesPageIndex);
-  }, []);
-  
-  return (
-    <main className="warehouse">
+function Warehouses({setNavIndex}) {
+    useEffect(() => {
+        setNavIndex(warehousesPageIndex);
+    }, []);
 
-    </main>
-  );
-};
+    const {id} = useParams();
+    const [warehouse, setWarehouse] = useState([]);
 
-export default Warehouse;
+    const fetchWarehouse = async () => {
+        try {
+            const warehouseResponse = await axios.get(`${warehousesEndpoint}/${id}`);
+            setWarehouse(warehouseResponse.data);
+        } catch (error) {
+            console.log(`Could not load warehouses: ${error}`);
+        }
+    }
+
+    useEffect( () => { fetchWarehouse(); }, []);
+    console.log(warehouse);
+
+    return (
+        <main className="warehouses">
+            <div className="warehouses__page-background">
+
+            </div>
+            <div className="warehouses__page-foreground">
+                <HeroWhDetails heroTitle={warehouse.warehouse_name} id={id}/>
+                <section className="warehouses__table">
+                    <TableHeaderWhDetails />
+                </section>
+            </div>
+        </main>
+    );
+}
+
+export default Warehouses;
