416c3a3ad2498a23fb98501b1a97ffc58f16fe3e
Ali Hamood <ali.hamood@gov.bc.ca>
2025-03-30 18:20:47 -0700

    Adds Warehouse page and mods old Warehouse to be Warehouses.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import './styles/app.scss';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
-import Warehouse from './pages/Warehouse/Warehouse';
+import Warehouses from './pages/Warehouses/Warehouses';
 import AddWarehouse from './pages/AddWarehouse/AddWarehouse';
+import Warehouse from './pages/Warehouse/Warehouse';
 import EditWarehouse from './pages/EditWarehouse/EditWarehouse';
 import DeleteWarehouse from './pages/DeleteWarehouse/DeleteWarehouse';
 import Inventory from './pages/Inventory/Inventory';
@@ -15,8 +16,9 @@ function App() {
       <BrowserRouter>
         <Header/>
         <Routes>
-            <Route path="/" element={ <Warehouse /> }/>
+            <Route path="/" element={ <Warehouses /> }/>
             <Route path="/warehouses/add-warehouse" element={ <AddWarehouse /> }/>
+            <Route path="/warehouses/:id" element={ <Warehouse /> }/>
             <Route path="/warehouses/:id/edit-warehouse" element={ <EditWarehouse /> }/>
             <Route path="/warehouses/:id/delete-warehouse" element={ <DeleteWarehouse /> }/>
             <Route path="/inventory" element={ <Inventory /> }/>
--- src/pages/Warehouse/Warehouse.jsx
+++ src/pages/Warehouse/Warehouse.jsx
@@ -1,48 +1,11 @@
-import './warehouse.scss';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import { warehousesEndpoint } from '../../data/apiData';
-import { v4 as uuidv4 } from 'uuid';
-import Hero from '../../components/Hero/Hero';
-import TableHeader from '../../components/TableHeader/TableHeader';
-import TableRow from '../../components/TableRow/TableRow';
+import './Warehouse.scss';
 
 function Warehouse() {
-    const [warehouses, setWarehouses] = useState([]);
+  return (
+    <main className="warehouse">
 
-    const fetchWarehouses = async () => {
-        try {
-            const warehousesResponse = await axios.get(warehousesEndpoint);
-            setWarehouses(warehousesResponse.data);
-        } catch (error) {
-            console.log(`Could not load warehouses: ${error}`);
-        }
-    }
-
-    const renderWarehouses = () => {
-        return warehouses.map((warehouse) => {
-            return (
-                <TableRow warehouse={warehouse} key={uuidv4()}/>
-            );
-        });
-    }
-
-    useEffect( () => { fetchWarehouses(); }, []);
-
-    return (
-        <main className="warehouse">
-            <div className="warehouse__page-background">
-
-            </div>
-            <div className="warehouse__page-foreground">
-                <Hero />
-                <section className="warehouse__table">
-                    <TableHeader />
-                    {renderWarehouses()}
-                </section>
-            </div>
-        </main>
-    );
-}
+    </main>
+  );
+};
 
 export default Warehouse;
