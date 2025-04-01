8d1de30433d06264f49fb18142ee2b286a0f8cc7
Ali Hamood <ali.hamood@gov.bc.ca>
2025-03-31 17:24:06 -0700

    Minor bug fixed introduced after the last commit.

--- src/components/TableHeaderLabel/TableHeaderLabel.jsx
+++ src/components/TableHeaderLabel/TableHeaderLabel.jsx
@@ -4,7 +4,7 @@ import SortIcon from '../../assets/icons/sort-24px.svg';
 
 function TableHeaderLabel({onClick, labelText}) {
     return (
-        <div className="table-header-label__column-label">
+        <div className="table-header-label">
             <label className="table-header-label__label">{labelText}</label>
             <Button imgSrc={SortIcon} buttonType="icon-only" onClick={onClick}/>
         </div>
