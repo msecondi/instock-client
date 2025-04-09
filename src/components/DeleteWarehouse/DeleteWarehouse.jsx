b6bbe7d798e64e1d240b14f63fae3beea084d32f
Ali Hamood <ali.hamood@gov.bc.ca>
2025-04-09 11:30:36 -0700

    Adds fetch warehouse or inventory name to delete model popup.

--- src/components/DeleteWarehouse/DeleteWarehouse.jsx
+++ src/components/DeleteWarehouse/DeleteWarehouse.jsx
@@ -1,8 +1,67 @@
 import './deleteWarehouse.scss';
+import { useLocation, useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import {warehousesEndpoint, inventoriesEndpoint } from '../../data/appData.json';
 import Button from '../Button/Button';
 import CloseIcon from '../../assets/icons/close-24px.svg';
 
 function DeleteWarehouse() {
+  const [messageHeading, setMessageHeading] = useState('');
+  const [messageText, setMessageText] = useState('');
+
+  const isWarehouse = useLocation().pathname.includes('warehouse');
+  const isInventory = useLocation().pathname.includes('inventories');
+  const id = useParams().id;
+
+  const setWarehouseMessage = async () => {
+    try {
+      const warehouseResponse = await axios.get(`${warehousesEndpoint}/${id}`);
+      const warehouse = warehouseResponse.data;
+
+      if (warehouse == null || warehouse.length === 0) {
+        throw new Error(`Warehouse with id ${id} does not exist. warehouse: ${warehouse}`);
+      }
+
+      setMessageHeading(`Delete ${warehouse.warehouse_name} warehouse?`);
+      setMessageText(`Please confirm that you'd like to delete the ${warehouse.warehouse_name} warehouse from the list of warehouses. You won't be able to undo this action.`);
+
+    } catch (error) {
+      console.log(`Could not load item id: ${id}, error: ${error.message}`);
+      setMessageHeading('Error');
+      setMessageText(`Could not load warehouse.`);
+    }
+  }
+
+  const setInventoryMessage = async () => {
+    try {
+      const warehouseResponse = await axios.get(`${warehousesEndpoint}/${id}`);
+      const warehouse = warehouseResponse.data;
+
+      if (warehouse == null || warehouse.length === 0) {
+        throw new Error(`Warehouse with id ${id} does not exist. warehouse: ${warehouse}`);
+      }
+
+      setMessageHeading(`Delete ${warehouse.warehouse_name} warehouse?`);
+      setMessageText(`Please confirm that you'd like to delete the ${warehouse.warehouse_name} warehouse from the list of warehouses. You won't be able to undo this action.`);
+
+    } catch (error) {
+      console.log(`Could not load item id: ${id}, error: ${error.message}`);
+      setMessageHeading('Error');
+      setMessageText(`Could not load warehouse.`);
+    }
+  }
+
+  useEffect(() => {
+    if (isWarehouse) {  
+      setWarehouseMessage();
+    }
+
+    if (isInventory) {  
+      setWarehouseMessage();
+    }
+      
+  }, []);
 
   return (
     <div className="delete-warehouse">
@@ -11,10 +70,10 @@ function DeleteWarehouse() {
       </div>
       <div className="delete-warehouse__message">
         <h2 className="delete-warehouse__message-header">
-          Are you sure you want to delete this warehouse?
+          { messageHeading }
         </h2>
         <p className="delete-warehouse__message-text">
-          This action cannot be undone.
+          { messageText }
         </p>
       </div>
       <div className="delete-warehouse__buttons">
