d4efbaa094a3b980c394afb317ad1516808b2de7
Ali Hamood <ali.hamood@gov.bc.ca>
2025-04-08 18:01:09 -0700

    Started styling the delte box.

--- src/components/DeleteWarehouse/DeleteWarehouse.jsx
+++ src/components/DeleteWarehouse/DeleteWarehouse.jsx
@@ -1,14 +1,25 @@
 import './deleteWarehouse.scss';
+import Button from '../Button/Button';
+import CloseIcon from '../../assets/icons/close-24px.svg';
 
 function DeleteWarehouse() {
 
   return (
     <div className="delete-warehouse">
-      <h2>Are you sure you want to delete this warehouse?</h2>
-      <p>This action cannot be undone.</p>
+      <div className="delete-warehouse__close-button">
+        <Button buttonText="" buttonType="icon-only" imgSrc={CloseIcon}/>
+      </div>
+      <div className="delete-warehouse__message">
+        <h2 className="delete-warehouse__message-header">
+          Are you sure you want to delete this warehouse?
+        </h2>
+        <p className="delete-warehouse__message-text">
+          This action cannot be undone.
+        </p>
+      </div>
       <div className="delete-warehouse__buttons">
-        <button className="delete-warehouse__button delete-warehouse__button--cancel">Cancel</button>
-        <button className="delete-warehouse__button delete-warehouse__button--delete">Delete</button>
+        <Button buttonText="Cancel" buttonType="delete"/>
+        <Button buttonText="Delete" buttonType="secondary"/>
       </div>
     </div>
   );
