0058d93cbf3ab7c79031811d2816fa66c839f6bc
Ali Hamood <ali.hamood@gov.bc.ca>
2025-04-18 08:13:58 -0700

    Search input now goes blue when active.

--- src/components/SearchFormField/SearchFormField.jsx
+++ src/components/SearchFormField/SearchFormField.jsx
@@ -1,10 +1,17 @@
 import './searchFormField.scss';
+import { useState } from 'react';
 import SearchIcon from '../../assets/icons/search-24px.svg';
 
 const SearchFormField = () => {
+    const [isActive, setIsActive] = useState(false);
+    
+    const inputOnFocus = () => {
+        setIsActive(true);
+    }
+
     return (
-        <div className="search search--right-margin">
-            <input className="search__input" type="text" placeholder="Search..." /> 
+        <div className={`search search--right-margin ${isActive ? 'search--active-border' : ''}`}>
+            <input className="search__input" type="text" placeholder="Search..." onFocus={inputOnFocus}/> 
             <button className="search__button">
                 <img className="search__icon" src={SearchIcon} alt="Search Icon" />
             </button>
