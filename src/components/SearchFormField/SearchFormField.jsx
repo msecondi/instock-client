d48a0ad7610364bd5a88dee92614891321d542f7
matt secondi <matt_secondi@hotmail.com>
2025-04-29 21:45:42 -0400

    added search functionality

--- src/components/SearchFormField/SearchFormField.jsx
+++ src/components/SearchFormField/SearchFormField.jsx
@@ -1,18 +1,87 @@
 import './searchFormField.scss';
-import { useState } from 'react';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { warehousesEndpoint } from "../../data/appData.json";
 import SearchIcon from '../../assets/icons/search-24px.svg';
 
-const SearchFormField = () => {
+const SearchFormField = ({searchContext, handleClick}) => {
+    const [searchText, setSearchText] = useState('');
+    const [baseData, setBaseData] = useState([]);
+    const [currentData, setCurrentData] = useState([]);
     const [isActive, setIsActive] = useState(false);
-
+    
     const inputOnFocus = () => {
-        setIsActive(!isActive);
+        setIsActive(true);
     }
+    const inputOnBlur = () => {
+        setIsActive(false);
+    }
+    
+    const search = (event) => {
+        console.log(event.target.value);
+        setSearchText(event.target.value)
+    }
+    const handleEnter = (event) => {
+        if (event.key === 'Enter') {
+            // Action to perform when Enter is pressed
+            handleClick(currentData);
+            // Call a function or update state as needed
+          }
+    }
+
+    //create warehouses state variable to convert ids to names when rendering inventories
+    const [warehouses, setWarehouses] = useState([]);
+
+    useEffect(() => {
+        const fetchWarehouses = async() => {
+            const response = await axios.get(`${warehousesEndpoint}`);
+            setWarehouses(response.data);
+        }
+        fetchWarehouses();
+    }, [])
+
+    useEffect(() => {
+        console.log(searchContext)
+        //if warehouse_id exists, then we are loading inventories
+        //therefore we need to convert warehouse_id into a usable name for the user to search through
+        if (searchContext[0]?.warehouse_id && warehouses.length > 0) {
+            //find id in warehouses and enrich data to also contain the associated warehouse_name
+            const enrichedData = searchContext.map(inventoryObj => {
+                //within the loop, at each individual inventory object, find and assign matching ids and store in variable
+                const warehouse = warehouses.find(warehouseObj => warehouseObj.id === inventoryObj.warehouse_id);
+                //return 'searchContext' but with an additional 'warehouse_name' key with associated data
+                return {
+                    ...inventoryObj,
+                    warehouse_name: warehouse?.warehouse_name || 'Unknown'
+                };
+            });
+            setBaseData(enrichedData);
+            setCurrentData(enrichedData);
+        } else {
+            setBaseData(searchContext);
+            setCurrentData(searchContext);
+          }
+    }, [searchContext, warehouses])
+
+    useEffect(() => {
+        console.log(currentData)
+        if (!searchText.trim()) {
+            setCurrentData(baseData); // Show all if empty
+            return;
+        }
+        const filtered = baseData.filter(obj => {
+            return Object.values(obj).some(val =>
+                val.toString().toLowerCase().includes(searchText.toLowerCase())
+            );
+        });
+        
+        setCurrentData(filtered);
+    }, [searchText, baseData]);
 
     return (
         <div className={`search ${isActive ? 'search--active-border' : ''}`}>
-            <input className="search__input" type="text" placeholder="Search..." onFocus={inputOnFocus} onBlur={inputOnFocus}/> 
-            <button className="search__button">
+            <input className="search__input" type="text" placeholder="Search..." onFocus={inputOnFocus} onBlur={inputOnBlur} onChange={search} onKeyDown={handleEnter}/> 
+            <button className="search__button" type="button" onClick={() => handleClick(currentData)}>
                 <img className="search__icon" src={SearchIcon} alt="Search Icon" />
             </button>
         </div>
